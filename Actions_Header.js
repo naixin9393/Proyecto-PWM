4dbc7aa2263397fc8ee2fabea5240c7b64b6c1df
ElenaFM <136226475+efm092000@users.noreply.github.com>
2024-02-20 10:16:59 +0000

    Update and rename actions.js to Actions_Header.js

--- Actions_Header.js
+++ Actions_Header.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function(){
     
     var MisTopsButton = document.getElementById('Button_Discover');
     MisTopsButton.addEventListener('click', function(){
-        window.location.href='prueba.html';
+        window.location.href='tops.html';
     })
 
     var DiscoverButton = document.getElementById('Button_Mis_Tops');

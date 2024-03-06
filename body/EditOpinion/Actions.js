f1f94471dadc9893d8dcc89e217284656569f05f
ElenaFM <136226475+efm092000@users.noreply.github.com>
2024-03-06 19:06:46 +0000

    Update Actions.js

--- body/EditOpinion/Actions.js
+++ body/EditOpinion/Actions.js
@@ -1,54 +1,5 @@
 
     document.addEventListener("DOMContentLoaded", function(){
-        document.addEventListener("DOMContentLoaded", function(){
-            var savedText = localStorage.getItem("savedText");
-            var savedSelection= localStorage.getItem("savedSelection");
-            
-            var savedopinionarea= document.getElementById("Opinion_Text_Area");
-            if(savedopinionarea){
-                savedopinionarea.value = savedText;
-            }
-        });
-
-        var MisTopsButton = document.getElementById('Button_Discover');
-        if(MisTopsButton){
-            MisTopsButton.addEventListener('click', function(){
-                window.location.href='tops.html';
-            });
-        }
-    
-        var DiscoverButton = document.getElementById('Button_Mis_Tops');
-        if(DiscoverButton){
-            DiscoverButton.addEventListener('click', function(){
-                window.location.href='tops.html';
-            });
-        }
-
-        var RankingsButton = document.getElementById('Button_Rankings');
-        if(RankingsButton){
-            RankingsButton.addEventListener('click', function(){
-                window.location.href='prueba.html';
-            });
-        }
-        
-        var LoginButton = document.getElementById('Button_Log_in');
-        if(LoginButton){
-            LoginButton.addEventListener('click', function(){
-                window.location.href='prueba.html';
-            });
-        }
-    });
-    
-    document.addEventListener("DOMContentLoaded",function(){
-        var EditButton = document.getElementById('Button_Edit');
-        if(EditButton){
-            EditButton.addEventListener('click', function(){
-                window.location.href='EditOpinionPage.html';
-            });
-        }
-    });
-    
-    document.addEventListener("DOMContentLoaded", function(){
         var DoneButton = document.getElementById('Button_Done');
         if(DoneButton){
             DoneButton.addEventListener('click', function(){

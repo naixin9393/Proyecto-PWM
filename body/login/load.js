737f1f8e0798a40446ae5c18f8e9aa290440df60
naixin9393 <naicinchen@gmail.com>
2024-03-06 20:59:50 +0000

    feat: add path from login to register

--- body/login/load.js
+++ body/login/load.js
@@ -3,28 +3,54 @@ import {loadTemplate, replaceBody} from '/loadingFunctions.js';
 export async function loadLoginBody() {
     let page = await loadTemplate('/body/login/login.html');
     page.querySelector('.landscape-image').src = '/images/sample.jpg';
-    let form = await loadLoginForm();
-    page.querySelector('.login-form').appendChild(form);
+
+    let formContainer = page.querySelector('.form-container');
+    await loadLoginForm(formContainer);
     replaceBody(page);
 }
 
-async function loadLoginForm() {
+async function loadLoginForm(formContainer) {
     let loginForm = await loadTemplate('/body/login/form.html');
     loadTitle(loginForm, 'Login');
     loadButton(loginForm, 'Login');
+
+    let question = loginForm.querySelector('.question-text');
+    question.textContent = 'Don\'t have an account?';
+    question.addEventListener('click', async function() {
+        await loadRegisterForm(formContainer);
+    });
     await loadUsernameTextField(loginForm);
     await loadPasswordTextField(loginForm, 'Password');
-    return loginForm;
+
+    if (formContainer.firstChild !== null) {
+        formContainer.replaceChild(loginForm, formContainer.firstChild);
+    } else {
+        formContainer.appendChild(loginForm);
+    }
+
 }
 
-async function loadRegisterForm() {
+async function loadRegisterForm(formContainer) {
     let registerForm = await loadTemplate('/body/login/form.html');
     loadTitle(registerForm, 'Register');
     loadButton(registerForm, 'Register');
+
+    let question = registerForm.querySelector('.question-text');
+    question.textContent = 'Already have an account?';
+
+    question.addEventListener('click', async function() {
+        await loadLoginForm(formContainer);
+    });
+
     await loadUsernameTextField(registerForm);
     await loadPasswordTextField(registerForm, 'Password');
     await loadPasswordTextField(registerForm, 'Confirm password');
-    return registerForm;
+
+    if (formContainer.firstChild !== null) {
+        formContainer.replaceChild(registerForm, formContainer.firstChild);
+    } else {
+        formContainer.appendChild(registerForm);
+    }
 }
 
 function loadTitle(loginForm, text) {
@@ -48,7 +74,8 @@ async function loadPasswordTextField(loginForm, text) {
 }
 
 function loadButton(loginForm, text) {
-    let loginButton = document.createElement('button');
-    loginButton.textContent = text;
-    loginForm.appendChild(loginButton);
+    let button = document.createElement('button');
+    button.className = 'form-button';
+    button.textContent = text;
+    loginForm.querySelector('.button-container').appendChild(button);
 }

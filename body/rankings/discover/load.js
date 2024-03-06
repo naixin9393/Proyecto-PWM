40b8d04ed0ff127e62c06ddf0bc03d256bd087e0
PabloSeguraLopez <pabseglop@gmail.com>
2024-03-06 20:22:40 +0000

    My rankings and discover added to header

--- body/rankings/discover/load.js
+++ body/rankings/discover/load.js
@@ -1,9 +1,22 @@
 import {loadTemplate, replaceBody} from '/loadingFunctions.js';
+import { loadTopRankingBody } from "/body/rankings/top_ranking/load.js";
+
 document.addEventListener('DOMContentLoaded', async function() {
     await loadDiscoverBody();
 });
 
 export async function loadDiscoverBody() {
     let discoverPage = await loadTemplate('/body/rankings/discover/discover_page.html');
+    await prepareLinks(discoverPage);
     replaceBody(discoverPage);
 }
+async function prepareLinks(discoverPage) {
+    var categories = document.getElementsByClassName("discovery_category");
+    for (let i = 0; i < categories.length; i++) {
+        categories.item(i).addEventListener('click',
+            async function () {
+                await loadTopRankingBody();
+            });
+    }
+}
+
--- header/load.js
+++ header/load.js
@@ -1,6 +1,8 @@
 import { loadTemplate } from '/loadingFunctions.js';
 import { loadLoginBody } from "/body/login/load.js";
 import { loadHomeBody } from "/body/home/load.js";
+import {loadDiscoverBody} from "../body/rankings/discover/load.js";
+import {loadMyRankingsBody} from "../body/rankings/my_rankings/load.js";
 
 document.addEventListener('DOMContentLoaded', async function() {
         let header = await loadTemplate('/header/Header.html');
@@ -16,5 +18,11 @@ function loadButtonsActions() {
 
     document.getElementById('Button_Mis_Tops').addEventListener('click', async function() {
         await loadHomeBody();
+    });
+    document.getElementById('Button_Discover').addEventListener('click', async function() {
+        await loadDiscoverBody();
+    });
+    document.getElementById('Button_Rankings').addEventListener('click', async function() {
+        await loadMyRankingsBody();
     });
 }

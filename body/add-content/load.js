15e05a33ef27cf8fdb9bcdef588e63570539bd32
naixin <naixin.chen101@alu.ulpgc.es>
2024-03-23 20:34:42 +0000

    style: add responsive add content page

--- body/add-content/load.js
+++ body/add-content/load.js
@@ -1,80 +1,33 @@
-import { loadTemplate, replaceBody, fetchJson } from "../../loadingFunctions.js";
+import { loadTemplate, replaceBody } from "../../loadingFunctions.js";
 import { loadDetailBody } from "../detail/load.js";
 
 export async function loadAddContentBody() {
     let page = await loadTemplate('/body/add-content/add-content.html');
-    loadTitle(page, 'Add Book/Movie/Series');
-    page.querySelector('.landscape-image').src = '/images/sample.jpg';
     let form = await loadForm();
     page.querySelector('.add-content-form').appendChild(form);
     replaceBody(page);
 }
 
-async function loadTitleLabel(form) {
-    let label = document.createElement('label');
-    label.textContent = 'Title: title';
-    label.className = 'input-label';
-    form.querySelector('.input-container').appendChild(label);
-}
-
-async function loadAuthorLabel(form) {
-    let label = document.createElement('label');
-    label.textContent = 'Author: author';
-    label.className = 'input-label';
-    form.querySelector('.input-container').appendChild(label);
-}
-
 async function loadForm() {
     let form = await loadTemplate('/body/add-content/form.html');
-    await loadTitleLabel(form);
-    await loadAuthorLabel(form);
-    loadReviewField(form);
-    await loadComboBox(form, 'score');
-    loadButtonContainer(form);
+    loadButtonAction(form);
+    loadComboBox(form);
     return form;
 }
 
-function loadTitle(page, text) {
-    let title = page.querySelector('.add-content-title');
-    title.textContent = text;
-}
-
-async function loadComboBox(form, data) {
-    let items = await fetchJson(`/body/add-content/data/${data}.json`);
-    let comboBoxTemplate = await loadTemplate('/body/add-content/combo-box/combo-box.html');
-    let comboBox = comboBoxTemplate.querySelector('.combo-box');
-    for (let item of items) {
+function loadComboBox(form) {
+    let select = form.querySelector('#add-content-score-select');
+    for (let i = 1; i < 6; i++) {
         let option = document.createElement('option');
-        option.value = item.name;
-        option.textContent = item.name;
-        comboBox.appendChild(option);
+        option.value = i;
+        option.textContent = i + " ⭐";
+        select.appendChild(option);
     }
-    form.querySelector('.input-container').appendChild(comboBox);
 }
 
-function loadReviewField(form) {
-    let review = document.createElement('textarea');
-    review.placeholder = 'Write a review';
-    review.id = 'add-content-review-textarea';
-    form.querySelector('.input-container').appendChild(review);
-}
-
-function loadButtonContainer(form) {
-    let buttonContainer = document.createElement('div');
-    buttonContainer.classList.add('button-container');
-
-    let cancelButton = document.createElement('button');
-    cancelButton.id = 'add-content-cancel-button';
-    cancelButton.textContent = 'Cancel';
+function loadButtonAction(form) {
+    let cancelButton = form.querySelector('#add-content-cancel-button');
     cancelButton.addEventListener('click', function() {
         loadDetailBody();
     });
-
-    let addToRankingButton = document.createElement('button');
-    addToRankingButton.id = 'add-content-add-to-ranking-button';
-    addToRankingButton.textContent = 'Add to your ranking';
-
-    buttonContainer.appendChild(cancelButton);
-    buttonContainer.appendChild(addToRankingButton);
-    form.querySelector('.button-container').appendChild(buttonContainer);
 }

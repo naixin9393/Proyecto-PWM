83911695c57fc68d5182a332368ad9becfb1be92
ElenaFM <efm092000@gmail.com>
2024-04-04 12:05:12 +0100

    Added component opinionpage

---
+++ src/app/opinionpage/opinionpage.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import {NgOptimizedImage} from "@angular/common";
+
+@Component({
+  selector: 'app-opinionpage',
+  standalone: true,
+  imports: [
+    NgOptimizedImage
+  ],
+  templateUrl: './opinionpage.component.html',
+  styleUrl: './opinionpage.component.css'
+})
+export class OpinionpageComponent {
+
+}

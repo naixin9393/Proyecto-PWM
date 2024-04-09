741407c842071df4ea5355ce5f2eb34e72f11d41
ElenaFM <efm092000@gmail.com>
2024-04-09 10:32:08 +0100

    Changed opinion component, esditing mode

--- src/app/components/opinionpage/opinionpage.component.ts
+++ src/app/components/opinionpage/opinionpage.component.ts
@@ -1,20 +1,37 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {NgOptimizedImage} from "@angular/common";
+import {HttpClient, HttpClientModule} from "@angular/common/http";
 
 
+
+// @ts-ignore
 @Component({
   selector: 'app-opinionpage',
   standalone: true,
   templateUrl: "opinionpage.component.html",
   imports: [
-    NgOptimizedImage
+    NgOptimizedImage,
+    HttpClientModule
   ],
   styleUrl: 'opinionpage.component.css'
 })
-export class OpinionpageComponent {
+export class OpinionpageComponent implements OnInit{
   editingMode: boolean = false;
   bookTitle: string ="Book Title";
   titleText: string = 'Your review'
+
+  constructor(private http: HttpClient){}
+  ngOnInit(): void {
+    this.http.get<any>('/assets/data/book-data.json').subscribe(
+      (data) => {
+        this.bookTitle = data.title;
+      },
+      (error) => {
+        console.error('Error loading book data:', error);
+      }
+    );
+  }
+
 
   toggleEditMode() {
     this.editingMode = !this.editingMode;

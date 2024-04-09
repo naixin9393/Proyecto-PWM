962fe3c9b666699a139e1ce20b886ca6ca0a5a1b
ElenaFM <efm092000@gmail.com>
2024-04-09 10:21:48 +0100

    Changed opinion component, esditing mode

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import {OpinionpageComponent} from "./components/opinionpage/opinionpage.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'opinion', component: OpinionpageComponent }
+];
+
--- src/app/components/opinionpage/opinionpage.component.ts
+++ src/app/components/opinionpage/opinionpage.component.ts
@@ -1,15 +1,23 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {NgOptimizedImage} from "@angular/common";
+
 
 @Component({
   selector: 'app-opinionpage',
   standalone: true,
+  templateUrl: "opinionpage.component.html",
   imports: [
     NgOptimizedImage
   ],
-  templateUrl: './opinionpage.component.html',
-  styleUrl: './opinionpage.component.css'
+  styleUrl: 'opinionpage.component.css'
 })
 export class OpinionpageComponent {
+  editingMode: boolean = false;
+  bookTitle: string ="Book Title";
+  titleText: string = 'Your review'
 
+  toggleEditMode() {
+    this.editingMode = !this.editingMode;
+    this.titleText = this.editingMode ? 'Edit your review' : 'Your review';
+  }
 }

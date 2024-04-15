e2cc4136f6ded002bf9663fb7480d0164a9de821
naixin <naixin.chen101@alu.ulpgc.es>
2024-04-15 13:35:28 +0100

    refactor: delete unused files

--- src/app/components/detail/detail.component.ts
+++ src/app/components/detail/detail.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Content } from "../../interfaces/content";
-import { NgForOf, NgOptimizedImage } from "@angular/common";
+import { NgForOf, NgIf, NgOptimizedImage } from "@angular/common";
 import { PlatformService } from "../../services/platform.service";
 import { ReviewComponent } from "./review/review.component";
 import { ReviewService } from "../../services/review.service";
+import { ContentService } from "../../services/content.service";
 
 @Component({
   selector: 'app-detail',
@@ -11,33 +12,24 @@ import { ReviewService } from "../../services/review.service";
   imports: [
     NgOptimizedImage,
     ReviewComponent,
-    NgForOf
+    NgForOf,
+    NgIf
   ],
   templateUrl: './detail.component.html',
   styleUrl: './detail.component.css'
 })
 export class DetailComponent {
-  protected reviewIds: number[] | undefined;
+  protected reviewIds?: number[];
+  protected content?: Content;
 
-  constructor(private platformService: PlatformService, private reviewService: ReviewService) { }
+  constructor(private platformService: PlatformService, private reviewService: ReviewService, private contentService: ContentService) { }
 
   ngOnInit(): void {
     this.reviewIds = this.reviewService.getReviewsMock(this.contentId);
+    this.content = this.contentService.getContentMock(this.contentId);
   }
 
   contentId: number = 1;
-
-  contentDetails: Content = {
-    title: 'One Piece',
-    imageUrl: 'https://cdn.myanimelist.net/images/anime/6/73245.jpg',
-    synopsis: 'Gol D. Roger was known as the "Pirate King," the strongest and most infamous being to have sailed the Grand Line. The capture and execution of Roger by the World Government brought a change throughout the world. His last words before his death revealed the existence of the greatest treasure in the world, One Piece. It was this revelation that brought about the Grand Age of Pirates',
-    genres: ["Action", "Adventure", "Comedy", "Super Power", "Drama", "Fantasy", "Shounen"],
-    rating: 4,
-    releaseDate: new Date('1999-10-20'),
-    availableOn: ['Crunchyroll', 'Funimation', 'Netflix'],
-    episodes: 1000,
-    durationInMinutes: 24
-  }
 
   getPlatformUrl(platform: string): string {
     return this.platformService.getPlatformUrl(platform);

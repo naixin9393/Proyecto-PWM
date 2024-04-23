ec7d768739ed1b66d328da9ccee1e9749b10217d
PabloSeguraLopez <pabseglop@gmail.com>
2024-04-23 09:07:05 +0100

    Login and Sign-Up implemented

---
+++ src/app/components/sign-up/sign-up.component.ts
@@ -0,0 +1,73 @@
+import {Component} from '@angular/core';
+import {RouterLink} from "@angular/router";
+import {FormsModule} from "@angular/forms";
+import {NgIf, NgOptimizedImage} from "@angular/common";
+
+@Component({
+  selector: 'app-sign-up',
+  standalone: true,
+  imports: [
+    RouterLink,
+    FormsModule,
+    NgIf,
+    NgOptimizedImage
+  ],
+  templateUrl: './sign-up.component.html',
+  styleUrl: './sign-up.component.css'
+})
+export class SignUpComponent {
+  protected length = false;
+  protected uppercase = false;
+  protected lowercase = false;
+  protected number = false;
+  protected special = false;
+  protected passwords = false;
+  protected requirements = false;
+  protected password: string = "";
+  protected confirmPassword: string = "";
+
+  constructor() {
+  }
+
+  satisfyRequirements() {
+    this.length = this.satisfyLength();
+    this.uppercase = this.satisfyUppercase();
+    this.lowercase = this.satisfyLowercase();
+    this.number = this.satisfyNumber();
+    this.special = this.satisfySpecial();
+    this.passwords = this.satisfyPasswords();
+
+    this.requirements = this.length &&
+      this.uppercase &&
+      this.lowercase &&
+      this.number &&
+      this.special &&
+      this.passwords;
+  }
+
+  satisfyLength() {
+    return this.password.length >= 8;
+  }
+
+  satisfyUppercase() {
+    return /[A-Z]/.test(this.password);
+  }
+
+  satisfyLowercase() {
+    return /[a-z]/.test(this.password);
+  }
+
+  satisfyNumber() {
+    return /\d/.test(this.password);
+  }
+
+  satisfySpecial() {
+    return /[^A-Za-z0-9]/.test(this.password);
+  }
+
+  satisfyPasswords() {
+    return this.password === this.confirmPassword;
+  }
+}
+
+

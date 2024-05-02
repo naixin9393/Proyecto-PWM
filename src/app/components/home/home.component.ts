2acd7237d8cfc466f3a79f1bcc307d75bcb2465e
DanielFabelo <159779101+DanielFabelo@users.noreply.github.com>
2024-05-02 14:07:59 +0200

    Developed 1st version of HomeComponent

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,15 +1,37 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {RouterLink} from "@angular/router";
+import {Content} from "../../interfaces/content";
+import {SeriesService} from "../../services/series.service";
+import {MovieService} from "../../services/movie.service";
+import {BookService} from "../../services/book.service";
+import {NgForOf, NgIf, NgOptimizedImage} from "@angular/common";
+import {CarouselModule} from "ngx-bootstrap/carousel";
+
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
-    RouterLink
+    RouterLink,
+    NgForOf,
+    NgIf,
+    NgOptimizedImage,
+    CarouselModule
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+  topSeries: Content[] = [];
+  topBooks: Content[] = [];
+  topMovies: Content[] = [];
 
+  constructor(private serieService: SeriesService, private movieService: MovieService, private bookService: BookService) {
+  }
+
+  ngOnInit() {
+    this.serieService.getContents().subscribe(series => this.topSeries = series);
+    this.bookService.getContents().subscribe(books => this.topBooks = books);
+    this.movieService.getContents().subscribe(movies => this.topMovies = movies);
+  }
 }

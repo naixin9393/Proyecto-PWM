542430207c91688a36789271aa0b01a6af8b6501
DanielFabelo <159779101+DanielFabelo@users.noreply.github.com>
2024-05-03 18:29:56 +0200

    Dynamic functions added to profile component

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {NgIf} from "@angular/common";
+import {UserService} from "../../services/user.service";
+import {User} from "../../interfaces/user";
 
 @Component({
   selector: 'app-profile',
@@ -10,8 +12,20 @@ import {NgIf} from "@angular/common";
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
   editingMode: boolean = false;
+  user: User | null = {id:'', username:'', email:''};
+  constructor(private userService: UserService) {
+  }
+
+  ngOnInit() {
+    this.userService.getCurrentUser().then(user => {
+      this.user = user;
+    }).catch(error => {
+      console.error('Error al obtener el usuario:', error);
+    });
+  }
+
   toggleEditMode(){
     this.editingMode = !this.editingMode;
   }

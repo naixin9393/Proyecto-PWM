a647a0e80e4e88f8f1b254571c8511de77266f04
ElenaFM <efm092000@gmail.com>
2024-05-04 14:02:32 +0100

    Refactored all the buttons.

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,27 +1,24 @@
 import { Component, Renderer2 } from '@angular/core';
 import {Router, RouterLink} from "@angular/router";
+import {UserService} from "../../services/user.service";
+import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   standalone: true,
   imports: [
-    RouterLink
+    RouterLink,
+    NgIf
   ],
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent{
-  constructor(private router: Router, private renderer: Renderer2) {}
-
-  async loadProfileBody() {
-    await this.router.navigateByUrl('/profile');
-    this.closeDropdownMenu();
+  islogged: boolean = false;
+  constructor(private router: Router, private renderer: Renderer2, private userService: UserService) {
+    this.islogged = this.userService.getCurrentUser() != null;
   }
 
-  async loadOpinionBody() {
-    await this.router.navigateByUrl('/opinion');
-    this.closeDropdownMenu();
-  }
 
   async loadDiscoverBody() {
     await this.router.navigateByUrl('/discover');
@@ -47,5 +44,9 @@ export class HeaderComponent{
       const newDisplay = currentDisplay === 'none' ? 'block' : 'none';
       this.renderer.setStyle(dropdownMenu, 'display', newDisplay);
     }
+  }
+  logOut(){
+    this.userService.logout();
+    this.islogged = false;
   }
 }
